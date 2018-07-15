6bf013a7c0680a8c059b1da61bde319010971f0b
Thomas Rosen <6295638+thomasrosen@users.noreply.github.com>
2018-07-15 16:08:10 +0200

    changed "for_whom" to "who_is_welcome"

--- places.js
+++ places.js
@@ -7,7 +7,7 @@ var places = [
     "min_age": 14,
     "max_age": 27,
     "website": "http://www.anyway-koeln.de/",
-    "for_whom": ["queer", "gay", "lesbian", "trans", "undecided", "hetero-friends"],
+    "who_is_welcome": ["queer", "gay", "lesbian", "trans", "undecided", "hetero-friends"],
     "tags": ["youthcenter", "cafe", "bar"]
   }
 ];
