c72861a9ce7613f3d44d7013e36c98f6a29cdfd9
Thomas Rosen <6295638+thomasrosen@users.noreply.github.com>
2018-07-15 18:18:00 +0200

    added few places

--- places.js
+++ places.js
@@ -68,7 +68,7 @@ var places = [
     "max_age": -1,
     "website": "http://www.gerede-dresden.de/",
     "this_is_a_place_for": ["queer", "undecided", "hetero-friends", "family"],
-    "tags": ["education"]
+    "tags": ["community-center", "education"]
   },
   {
     "name": "GAP",
@@ -80,5 +80,137 @@ var places = [
     "website": "http://gap-in-bonn.de",
     "this_is_a_place_for": ["queer", "undecided", "hetero-friends", "family"],
     "tags": ["youthcenter", "education"]
+  },
+  {
+    "name": "Harvey House",
+    "lat": 41.692108,
+    "lng": -83.5785127,
+    "address": "2039 W Laskey Rd, Toledo, OH 43612, USA",
+    "min_age": -1,
+    "max_age": -1,
+    "website": "http://harveyhousenwo.org/",
+    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "tags": ["youthcenter"]
+  },
+  {
+    "name": "ITrouth - Trans-inter Jugendgruppe Oldenburg",
+    "lat": 53.1485385,
+    "lng": 8.2015414,
+    "address": "Ziegelhofstraße 83, 26121 Oldenburg",
+    "min_age": 14,
+    "max_age": 27,
+    "website": "https://twitter.com/itrouth",
+    "this_is_a_place_for": ["trans", "inter"],
+    "tags": ["youthgroup"]
+  },
+  {
+    "name": "Hempels",
+    "lat": 53.1485385,
+    "lng": 8.2015414,
+    "address": "Ziegelhofstraße 83, 26121 Oldenburg",
+    "min_age": -1,
+    "max_age": -1,
+    "website": "naund-oldenburg.de",
+    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "tags": ["community-center", "cafe", "bar"]
+  },
+  {
+    "name": "Jugendnetzwerk::Lambda::Berlin-Brandenburg e. V",
+    "lat": 52.54831,
+    "lng": 13.4049613,
+    "address": "Sonnenburger Str. 69, 10437 Berlin",
+    "min_age": 14,
+    "max_age": 27,
+    "website": "https://www.lambda-bb.de/",
+    "this_is_a_place_for": ["queer", "undecided"],
+    "tags": ["youthcenter"]
+  },
+  {
+    "name": "Queeres Zentrum Darmstadt",
+    "lat": 49.8830765,
+    "lng": 8.6651286,
+    "address": "Kranichsteiner Str. 81, 64289 Darmstadt",
+    "min_age": 14,
+    "max_age": 27,
+    "website": "http://queereszentrum-darmstadt.org/",
+    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "tags": ["youthcenter"]
+  },
+  {
+    "name": "Queeres Zentrum Göttingen",
+    "lat": 51.5575034,
+    "lng": 9.9298358,
+    "address": "Hannoversche Str. 80, 37077 Göttingen",
+    "min_age": -1,
+    "max_age": -1,
+    "website": "http://queeres-zentrum-goettingen.de",
+    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "tags": ["community-center", "youthcenter"]
+  },
+  {
+    "name": "Knutschfleck e.V.",
+    "lat": 50.7702529,
+    "lng": 6.0729973,
+    "address": "Jakobspl. 1, 52064 Aachen",
+    "min_age": 14,
+    "max_age": 27,
+    "website": "https://www.knutschfleck-online.de/",
+    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "tags": ["youthcenter"]
+  },
+  {
+    "name": "KUSS41",
+    "lat": 50.1136134,
+    "lng": 8.6854024,
+    "address": "Kurt-Schumacher-Straße 41, 60311 Frankfurt am Main",
+    "min_age": 14,
+    "max_age": 27,
+    "website": "http://www.kuss41.de/",
+    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "tags": ["youthcenter"]
+  },
+  {
+    "name": "Das Gugg – Café und Vereinszentrum der HOSI Wien",
+    "lat": 48.1962028,
+    "lng": 16.3595653,
+    "address": "Heumühlgasse 14, 1040 Wien",
+    "min_age": -1,
+    "max_age": -1,
+    "website": "http://www.hosiwien.at/gugg/",
+    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "tags": ["cafe"]
+  },
+  {
+    "name": "DéjàWü e. V.",
+    "lat": 49.79521,
+    "lng": 9.9178613,
+    "address": "Nigglweg 2, 97082 Würzburg",
+    "min_age": 16,
+    "max_age": 26,
+    "website": "http://www.dejawue.de/",
+    "this_is_a_place_for": ["queer", "undecided"],
+    "tags": ["youthcenter"]
+  },
+  {
+    "name": "LSBT-Jugendtreff Hildesheim",
+    "lat": 52.157187,
+    "lng": 9.952266,
+    "address": "Bernwardstraße 3, 31134 Hildesheim",
+    "min_age": 14,
+    "max_age": 24,
+    "website": "https://lsbt-jugendtreff-hildesheim.jimdo.com/",
+    "this_is_a_place_for": ["queer", "undecided"],
+    "tags": ["youthgroup"]
+  },
+  {
+    "name": "Queerbeet Hildesheim",
+    "lat": 51.9849145,
+    "lng": 9.8257856,
+    "address": "Am Klinsberg 1, 31061 Alfeld, Niedersachsen, Germany",
+    "min_age": -1,
+    "max_age": -1,
+    "website": "https://www.facebook.com/queerbeet.hildesheim/",
+    "this_is_a_place_for": ["queer", "undecided"],
+    "tags": ["group"]
   }
 ];
