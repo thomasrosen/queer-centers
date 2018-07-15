cef369969dd42a24180304fe7def7b762c84aff9
Thomas Rosen <6295638+thomasrosen@users.noreply.github.com>
2018-07-15 17:12:33 +0200

    added Magnus-Hirschfeld-Centrum
    Quellen:
    https://www.mhc-hh.de/%C3%BCber-uns-1/das-mhc/
    https://www.google.de/maps/place/Magnus-Hirschfeld-Centrum/@53.5893874,10.0139137,17z/data=!3m1!4b1!4m5!3m4!1s0x47b18927e08d80c3:0x14f8b6e28eeb9e04!8m2!3d53.5893842!4d10.0161025

--- places.js
+++ places.js
@@ -1,3 +1,19 @@
+/*
+template:
+  {
+    "name": "",
+    "lat": ,
+    "lng": ,
+    "address": "",
+    "min_age": -1,
+    "max_age": -1,
+    "website": "",
+    "this_is_a_place_for": [],
+    "tags": []
+  },
+
+*/
+
 var places = [
   {
     "name": "Anyway",
@@ -7,7 +23,18 @@ var places = [
     "min_age": 14,
     "max_age": 27,
     "website": "http://www.anyway-koeln.de/",
-    "this_is_a_place_for": ["queer", "gay", "lesbian", "trans", "undecided", "hetero-friends"],
+    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
     "tags": ["youthcenter", "cafe", "bar"]
-  }
+  },
+  {
+    "name": "Magnus-Hirschfeld-Centrum",
+    "lat": 53.5893874,
+    "lng": 10.0139137,
+    "address": "Borgweg 8, 22303 Hamburg, Germany",
+    "min_age": -1,
+    "max_age": -1,
+    "website": "https://www.mhc-hh.de/",
+    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "tags": ["cafe", "youthcenter"]
+  },
 ];
