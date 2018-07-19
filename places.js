705303f5056964e1df63cec55836eafbea3034bb
Thomas Rosen <6295638+thomasrosen@users.noreply.github.com>
2018-07-19 14:11:05 +0200

    Update places.js

--- places.js
+++ places.js
@@ -23,7 +23,7 @@ var places = [
     "min_age": 14,
     "max_age": 27,
     "website": "http://www.anyway-koeln.de/",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
     "tags": ["youthcenter", "cafe", "bar"]
   },
   {
@@ -34,7 +34,7 @@ var places = [
     "min_age": -1,
     "max_age": -1,
     "website": "https://www.mhc-hh.de/",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
     "tags": ["cafe", "youthcenter"]
   },
   {
@@ -45,7 +45,7 @@ var places = [
     "min_age": -1,
     "max_age": 23,
     "website": "http://www.sunrise-dortmund.de/",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
     "tags": ["youthcenter", "education"]
   },
   {
@@ -56,7 +56,7 @@ var places = [
     "min_age": -1,
     "max_age": 27,
     "website": "https://diversity-muenchen.de/",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
     "tags": ["youthcenter", "cafe", "education"]
   },
   {
@@ -67,7 +67,7 @@ var places = [
     "min_age": 14,
     "max_age": -1,
     "website": "http://www.gerede-dresden.de/",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends", "family"],
+    "this_is_a_place_for": ["queer", "undecided", "friends", "family"],
     "tags": ["community-center", "education"]
   },
   {
@@ -78,7 +78,7 @@ var places = [
     "min_age": 14,
     "max_age": 27,
     "website": "http://gap-in-bonn.de",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends", "family"],
+    "this_is_a_place_for": ["queer", "undecided", "friends", "family"],
     "tags": ["youthcenter", "education"]
   },
   {
@@ -89,7 +89,7 @@ var places = [
     "min_age": -1,
     "max_age": -1,
     "website": "http://harveyhousenwo.org/",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
     "tags": ["youthcenter"]
   },
   {
@@ -111,7 +111,7 @@ var places = [
     "min_age": -1,
     "max_age": -1,
     "website": "naund-oldenburg.de",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
     "tags": ["community-center", "cafe", "bar"]
   },
   {
@@ -133,7 +133,7 @@ var places = [
     "min_age": 14,
     "max_age": 27,
     "website": "http://queereszentrum-darmstadt.org/",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
     "tags": ["youthcenter"]
   },
   {
@@ -144,7 +144,7 @@ var places = [
     "min_age": -1,
     "max_age": -1,
     "website": "http://queeres-zentrum-goettingen.de",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
     "tags": ["community-center", "youthcenter"]
   },
   {
@@ -155,7 +155,7 @@ var places = [
     "min_age": 14,
     "max_age": 27,
     "website": "https://www.knutschfleck-online.de/",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
     "tags": ["youthcenter"]
   },
   {
@@ -166,7 +166,7 @@ var places = [
     "min_age": 14,
     "max_age": 27,
     "website": "http://www.kuss41.de/",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
     "tags": ["youthcenter"]
   },
   {
@@ -177,7 +177,7 @@ var places = [
     "min_age": -1,
     "max_age": -1,
     "website": "http://www.hosiwien.at/gugg/",
-    "this_is_a_place_for": ["queer", "undecided", "hetero-friends"],
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
     "tags": ["cafe"]
   },
   {
@@ -212,5 +212,159 @@ var places = [
     "website": "https://www.facebook.com/queerbeet.hildesheim/",
     "this_is_a_place_for": ["queer", "undecided"],
     "tags": ["group"]
+  },
+  {
+    "name": "All the same",
+    "lat": 52.2861503,
+    "lng": ,8.9063873,
+    "address": "Juxbude Minden, Königswall 101, 32423 Minden",
+    "min_age": 14,
+    "max_age": 21,
+    "website": "https://www.facebook.com/allthesameMinden/",
+    "this_is_a_place_for": ["queer", "undecided"],
+    "tags": ["youthcenter"]
+  },
+  {
+    "name": "Queerlaut Bielefeld",
+    "lat": 52.0741504,
+    "lng": 8.5937618,
+    "address": "AWO Kinder- und Jugendhaus Brake, Glückstädterstr. 25, 33729 Bielefeld",
+    "min_age": 16,
+    "max_age": 27,
+    "website": "https://www.queerlaut.de/",
+    "this_is_a_place_for": [],
+    "tags": ["group"]
+  },
+  {
+    "name": "jung, schwul, positiv Köln",
+    "lat": 50.9326734,
+    "lng": 6.9371913,
+    "address": "Aidshilfe Köln, Beethovenstraße 1, 50674 Köln",
+    "min_age": -1,
+    "max_age": 30,
+    "website": "http://queere-jugend-nrw.de/projekte/jung-schwul-positiv-koeln/",
+    "this_is_a_place_for": ["gay"],
+    "tags": ["group"]
+  },
+  {
+    "name": "Bunter Ort Wuppertal",
+    "lat": 51.2289002,
+    "lng": 7.1032164,
+    "address": "Kinder-und Jugendfarm Wuppertal e.V., Rutenbecker Weg 167, 42329 Wuppertal",
+    "min_age": 14,
+    "max_age": 27,
+    "website": "https://www.facebook.com/BunterOrt/",
+    "this_is_a_place_for": ["queer", "undecided", "hetero"],
+    "tags": ["youthcenter"]
+  },
+  {
+    "name": "Young Heroes Mönchengladbach",
+    "lat": 51.1962811,
+    "lng": 51.1962811,
+    "address": "Aidshilfe Mönchengladbach, August-Pieper-Straße 1, 41061 Mönchengladbach",
+    "min_age": 14,
+    "max_age": 21,
+    "website": "https://www.facebook.com/YoungHeroes-Young-Gay-M%C3%B6nchengladbach-805027019619523/",
+    "this_is_a_place_for": ["bi", "gay"],
+    "tags": ["youthcenter"]
+  },
+  {
+    "name": "Jugendtreff Paderborn",
+    "lat": 51.720063,
+    "lng": 8.7457264,
+    "address": "AIDS-Hilfe Paderborn, Friedrichstraße 51, 33102 Paderborn",
+    "min_age": 14,
+    "max_age": 26,
+    "website": "https://www.facebook.com/QueereJugendgruppePaderborn/",
+    "this_is_a_place_for": ["gay", "lesbian", "bi", "trans", "queer", "undecided"],
+    "tags": ["youthgroup"]
+  },
+  {
+    "name": "Trans*Fair Bonn",
+    "lat": 50.7401387,
+    "lng": 7.1233544,
+    "address": "GAP in Bonn, Obere Wilhelmstraße 29, 53225 Bonn-Beuel",
+    "min_age": 14,
+    "max_age": 27,
+    "website": "http://gap-in-bonn.de/",
+    "this_is_a_place_for": ["trans", "inter"],
+    "tags": ["youthgroup"]
+  },
+  {
+    "name": "Sunrise MeeTS Dortmund",
+    "lat": 51.5173371,
+    "lng": 7.467899,
+    "address": "Sunrise im Fritz-Henßler-Haus, Geschwister-Scholl-Straße 33-37, 44135 Dortmund",
+    "min_age": 14,
+    "max_age": 23,
+    "website": "http://www.sunrise-dortmund.de/",
+    "this_is_a_place_for": ["trans"],
+    "tags": ["youthgroup"]
+  },
+  {
+    "name": "Lucky MG Mönchengladbach",
+    "lat": 51.1962811,
+    "lng": 6.4220063,
+    "address": "Aidshilfe Mönchengladbach/Rheydt, August-Pieper-Straße 1, 41061 Mönchengladbach",
+    "min_age": 24,
+    "max_age": 27,
+    "website": "https://www.facebook.com/liveluckymg",
+    "this_is_a_place_for": ["bi", "lesbian"],
+    "tags": ["youthgroup"]
+  },
+  {
+    "name": "begin Bielefeld",
+    "lat": 52.0185625,
+    "lng": 8.5467641,
+    "address": "Jugendzentrum Kamp, Niedermühlenkamp 43, 33604 Bielefeld",
+    "min_age": 14,
+    "max_age": 26,
+    "website": "http://begin-bielefeld.de/",
+    "this_is_a_place_for": ["queer", "undecided"],
+    "tags": ["youthcenter"]
+  },
+  {
+    "name": "Diversity Detmold",
+    "lat": 51.9388606,
+    "lng": 8.8745995,
+    "address": "Jugendfreizeithaus, Wiesenstraße 5a, 32756 Detmold",
+    "min_age": 16,
+    "max_age": 25,
+    "website": "http://viva-detmold.de/diversity/",
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
+    "tags": ["youthcenter", "cafe"]
+  },
+  {
+    "name": "SLaM in Moers",
+    "lat": 51.4481739,
+    "lng": 6.6058028,
+    "address": "Keller des Kindergartens, Rüttgersweg 25, 47441 Moers",
+    "min_age": 16,
+    "max_age": 26,
+    "website": "http://slam-moers.de/",
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
+    "tags": ["youthgroup"]
+  },
+  {
+    "name": "MGay Mönchengladbach",
+    "lat": 51.1962811,
+    "lng": 6.4220063,
+    "address": "Aids-Hilfe Mönchengladbach, August-Pieper-Straße 1, 41061 Mönchengladbach",
+    "min_age": 25,
+    "max_age": -1,
+    "website": "https://www.facebook.com/MGay-Die-Gruppe-1618361998409829",
+    "this_is_a_place_for": ["gay"],
+    "tags": ["group"]
+  },
+  {
+    "name": "Puzzles im andersRoom Siegen",
+    "lat": 50.8805534,
+    "lng": 8.0054913,
+    "address": "andersROOM, Freudenbergerstraße 67, 57072 Siegen",
+    "min_age": 14,
+    "max_age": 24,
+    "website": "https://www.facebook.com/Puzzles-Jugendtreff-im-andersROOM-615735518573140",
+    "this_is_a_place_for": ["queer", "undecided", "friends"],
+    "tags": ["youthcenter"]
   }
 ];
